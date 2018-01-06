1693d4e0dfdcfef91072920503f8fc2116fb4c8c
Davanon <davanon2014@gmail.com>
2018-01-06 15:42:57 +0530

    First

--- src/main/java/com/iqmsoft/Application.java
+++ src/main/java/com/iqmsoft/Application.java
@@ -3,11 +3,24 @@ package com.iqmsoft;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.iqmsoft.datastructures.TreeApp;
+
+
+
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        
+    	
+    	
+    	SpringApplication.run(Application.class, args);
+        
+    	TreeApp a = new TreeApp();
+    	
+    	a.test();
+    	
+        
     }
 
 }
